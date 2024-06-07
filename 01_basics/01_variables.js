4b3819923c7f4bce4eff21fdf9cc8f0baaaf3a95
Yashika-Singh03 <myself.yashika03@gmail.com>
2024-06-07 10:09:29 +0000

    contain config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,16 @@
-console.log("Yashika")
+const accountId="12234"
+let accountEmail="abc@gmail.com"
+var accountPassword="11235"
+ accountCity="Aligarh"
+//  accountId=2
+ console.log(accountId);
+ accountEmail="xyz@gmail.com"
+ accountPassword="2134"
+ accountCity="Jhansi"
+ let accountState;
+
+ console.table([accountEmail,accountPassword,accountCity, accountId,accountState])
+ /*Prefer not to use var
+ because of issue in block scope and functional scope
+ */
+
